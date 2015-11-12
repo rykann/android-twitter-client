279f1c939894560f9e489c4b3fb6ce925cff91b5
Ryan Kannegiesser <ryank@groupon.com>
2015-11-11 22:46:08 -0600

    Add view pager and sliding tab string

--- app/src/main/java/org/kannegiesser/twitterclient/activities/TimelineActivity.java
+++ app/src/main/java/org/kannegiesser/twitterclient/activities/TimelineActivity.java
@@ -1,30 +1,34 @@
 package org.kannegiesser.twitterclient.activities;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.astuetz.PagerSlidingTabStrip;
+
 import org.kannegiesser.twitterclient.R;
 import org.kannegiesser.twitterclient.fragments.ComposeTweetDialog;
 import org.kannegiesser.twitterclient.fragments.HomeTimelineFragment;
+import org.kannegiesser.twitterclient.fragments.MentionsTimelineFragment;
 
 public class TimelineActivity extends AppCompatActivity implements ComposeTweetDialog.ComposeTweetDialogListener {
 
     private static final String TAG = "TimelineActivity";
-
-    HomeTimelineFragment homeTimelineFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
 
-        if (savedInstanceState == null) {
-            homeTimelineFragment = (HomeTimelineFragment) getSupportFragmentManager()
-                    .findFragmentById(R.id.timeline_fragment);
-        }
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new TweetsPagerAdapter(getSupportFragmentManager()));
+        PagerSlidingTabStrip tabStrip = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+        tabStrip.setViewPager(viewPager);
     }
 
     @Override
@@ -51,7 +55,36 @@ public class TimelineActivity extends AppCompatActivity implements ComposeTweetD
     @Override
     public void onTweetPosted() {
         //TODO: latest tweets should be added in a less brute force way
-        homeTimelineFragment.clear();
-        homeTimelineFragment.fetchTweets();
+        //homeTimelineFragment.clear();
+        //homeTimelineFragment.fetchTweets();
+    }
+
+    public class TweetsPagerAdapter extends FragmentPagerAdapter {
+        private String tabTitles[] = { "Home", "Mentions" };
+
+        public TweetsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            if (position == 0) {
+                return new HomeTimelineFragment();
+            } else if (position == 1) {
+                return new MentionsTimelineFragment();
+            } else {
+                return null;
+            }
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tabTitles[position];
+        }
+
+        @Override
+        public int getCount() {
+            return tabTitles.length;
+        }
     }
 }
