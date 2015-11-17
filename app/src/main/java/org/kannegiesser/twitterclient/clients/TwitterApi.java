40ef774a2cc6deb6e0827d8f1c3e751e847ca0e1
Ryan Kannegiesser <ryank@groupon.com>
2015-11-16 21:12:13 -0600

    Convert tabs to spaces

--- app/src/main/java/org/kannegiesser/twitterclient/clients/TwitterApi.java
+++ app/src/main/java/org/kannegiesser/twitterclient/clients/TwitterApi.java
@@ -10,73 +10,73 @@ import org.scribe.builder.api.Api;
 
 public class TwitterApi extends OAuthBaseClient {
 
-	private static final String TAG = TwitterApi.class.getName();
+    private static final String TAG = TwitterApi.class.getName();
 
-	public static final Class<? extends Api> REST_API_CLASS = org.scribe.builder.api.TwitterApi.class;
+    public static final Class<? extends Api> REST_API_CLASS = org.scribe.builder.api.TwitterApi.class;
 
-	public TwitterApi(Context context) {
-		super(context,
+    public TwitterApi(Context context) {
+        super(context,
                 REST_API_CLASS,
                 TwitterApiConfig.getBaseUrl(),
                 TwitterApiConfig.getConsumerKey(),
                 TwitterApiConfig.getConsumerSecret(),
                 TwitterApiConfig.getCallbackUrl());
-	}
+    }
 
-	public void getHomeTimeline(String maxId, AsyncHttpResponseHandler handler) {
-		String url = getApiUrl("statuses/home_timeline.json");
-		RequestParams params = new RequestParams();
-		params.put("count", 25);
+    public void getHomeTimeline(String maxId, AsyncHttpResponseHandler handler) {
+        String url = getApiUrl("statuses/home_timeline.json");
+        RequestParams params = new RequestParams();
+        params.put("count", 25);
         if (maxId != null) {
             params.put("max_id", maxId);
         }
-		client.get(url, params, handler);
-	}
+        client.get(url, params, handler);
+    }
 
-	public void getMentionsTimeline(String maxId, AsyncHttpResponseHandler handler) {
-		String url = getApiUrl("statuses/mentions_timeline.json");
-		RequestParams params = new RequestParams();
-		params.put("count", 25);
-		if (maxId != null) {
-			params.put("max_id", maxId);
-		}
-		client.get(url, params, handler);
-	}
+    public void getMentionsTimeline(String maxId, AsyncHttpResponseHandler handler) {
+        String url = getApiUrl("statuses/mentions_timeline.json");
+        RequestParams params = new RequestParams();
+        params.put("count", 25);
+        if (maxId != null) {
+            params.put("max_id", maxId);
+        }
+        client.get(url, params, handler);
+    }
 
-	public void getUserTimeline(String maxId, String screenName, AsyncHttpResponseHandler handler) {
-		String url = getApiUrl("statuses/user_timeline.json");
-		RequestParams params = new RequestParams();
-		params.put("count", 25);
-		params.put("screen_name", screenName);
-		if (maxId != null) {
-			params.put("max_id", maxId);
-		}
-		client.get(url, params, handler);
-	}
+    public void getUserTimeline(String maxId, String screenName, AsyncHttpResponseHandler handler) {
+        String url = getApiUrl("statuses/user_timeline.json");
+        RequestParams params = new RequestParams();
+        params.put("count", 25);
+        params.put("screen_name", screenName);
+        if (maxId != null) {
+            params.put("max_id", maxId);
+        }
+        client.get(url, params, handler);
+    }
 
-	public void getUserInfo(AsyncHttpResponseHandler handler) {
-		String url = getApiUrl("account/verify_credentials.json");
-		RequestParams params = new RequestParams();
+    public void getUserInfo(AsyncHttpResponseHandler handler) {
+        String url = getApiUrl("account/verify_credentials.json");
+        RequestParams params = new RequestParams();
         params.put("skip_status", "true");
-		client.get(url, params, handler);
-	}
+        client.get(url, params, handler);
+    }
 
-	public void getUserInfo(String screenName, AsyncHttpResponseHandler handler) {
-		// if no screen name given, fetch current user's profile
-		if (screenName == null) {
-			getUserInfo(handler);
-			return;
-		}
-		String url = getApiUrl("users/show.json");
-		RequestParams params = new RequestParams();
-		params.put("screen_name", screenName);
-		client.get(url, params, handler);
-	}
+    public void getUserInfo(String screenName, AsyncHttpResponseHandler handler) {
+        // if no screen name given, fetch current user's profile
+        if (screenName == null) {
+            getUserInfo(handler);
+            return;
+        }
+        String url = getApiUrl("users/show.json");
+        RequestParams params = new RequestParams();
+        params.put("screen_name", screenName);
+        client.get(url, params, handler);
+    }
 
-	public void postTweet(String text, AsyncHttpResponseHandler handler) {
-		String url = getApiUrl("statuses/update.json");
-		RequestParams params = new RequestParams();
-		params.put("status", text);
-		client.post(url, params, handler);
-	}
+    public void postTweet(String text, AsyncHttpResponseHandler handler) {
+        String url = getApiUrl("statuses/update.json");
+        RequestParams params = new RequestParams();
+        params.put("status", text);
+        client.post(url, params, handler);
+    }
 }
